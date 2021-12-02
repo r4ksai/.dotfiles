4b8e893d433768578ed16f1df6bb61692cb09afd
Sai <r4ksai@gmail.com>
2021-12-02 15:03:47 +0530

    Add code to install oh my zsh plugins 📩

--- setup.sh
+++ setup.sh
@@ -6,6 +6,8 @@ setup()
 {
 	# Oh-My-Zsh
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+	git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
 	ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 	# Git
