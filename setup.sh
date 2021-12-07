04c41059f8422ad7cf54591bfbdd6ab82fd9e109
Sai <r4ksai@gmail.com>
2021-12-08 04:13:38 +0530

    Add oh-my-zsh theme 🐋
    Add powerline theme for oh my zsh

--- setup.sh
+++ setup.sh
@@ -6,6 +6,7 @@ setup()
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 	ln -s ~/.dotfiles/.zshrc ~/.zshrc
 
 	# Git
