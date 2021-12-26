366b76be258f693f755b622307b52d527742c06a
Sai <r4ksai@gmail.com>
2021-12-26 09:50:46 +0530

    Add support for kali in setup 📚
    Remove syntax highlighting and autocomplete from setup installation

--- setup.sh
+++ setup.sh
@@ -34,9 +34,14 @@ setup()
   
 	# Oh-My-Zsh
 	sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-    # Remove this if using kali and import from patches
-	git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    if lsb_release -a | grep -Fxq "Kali"
+    then
+        echo "Using Kali"
+    else
+        # Remove this if using kali and import from patches
+        git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+        git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
+    fi
 
 	# Tmux
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
