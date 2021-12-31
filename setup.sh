3c596567f86823c12e408aa94192bebdadf8eaa7
Sai <r4ksai@gmail.com>
2021-12-31 23:42:51 +0530

    🍾 Add link error and mv error checks

--- setup.sh
+++ setup.sh
@@ -57,51 +57,80 @@ setup()
 	link
 }
 
-patchfiles()
-{
-	mkdir -p ~/.patches
-	cd ~/.patches
-
-	diff ~/.dotfiles/.vimrc ~/.vimrc > ~/.patches/.vimrc.patch
-	diff ~/.dotfiles/.tmux.conf ~/.tmux.conf > ~/.patches/.tmux.patch
-	diff ~/.dotfiles/.zshrc ~/.zshrc > ~/.patches/.zshrc.patch
-	diff ~/.dotfiles/.gitconfig ~/.gitconfig > ~/.patches/.gitconfig.patch
-	diff ~/.dotfiles/.vimrc ~/.config/nvim/init.vim > ~/.patches/.nvim.patch
-	
-	backup
-}
 
 backup()
 {
-	mv ~/.vimrc ~/.patches/.vimrc.backup
-	mv ~/.gitconfig ~/.patches/.gitconfig.backup 
-	mv ~/.zshrc ~/.patches/.zshrc.backup 
-	mv ~/.tmux.conf ~/.patches/.tmux.conf.backup 
-	mv ~/.config/nvim/init.vim ~./.patches/.nvim.backup
+	mkdir -p ~/.backups
+	cd ~/.backups
+
+    if [ -f ~/.vimrc ]; then 
+        mv ~/.vimrc ~/.backups/.vimrc.backup
+    fi
+    if [ -f ~/.gitconfig ]; then 
+        mv ~/.gitconfig ~/.backups/.gitconfig.backup 
+    fi
+    if [ -f ~/.zshrc ]; then 
+        mv ~/.zshrc ~/.backups/.zshrc.backup 
+    fi
+    if [ -f ~/.tmux.conf ]; then 
+        mv ~/.tmux.conf ~/.backups/.tmux.conf.backup 
+    fi
+    if [ -f ~/.config/nvim/init.vim ]; then 
+        mv ~/.config/nvim/init.vim ~./.backups/.nvim.backup
+    fi
 }
 
 link()
 {
-	# Create nvim directory
-	mkdir -p ~/.config/nvim
-	# Create backups
-	patchfiles
+    # Create nvim directory
+    mkdir -p ~/.config/nvim
 
-	# Link
-	ln -s ~/.dotfiles/.vimrc ~/.vimrc
-	ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-	ln -s ~/.dotfiles/.zshrc ~/.zshrc
-	ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-	ln -s ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
+    # Create backups
+    backup
+
+    # Link
+    ln -s ~/.dotfiles/.vimrc ~/.vimrc
+    ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+    ln -s ~/.dotfiles/.zshrc ~/.zshrc
+    ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+    ln -s ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
 }
 
 clean()
 {
-	rm ~/.vimrc
-	rm ~/.tmux.conf
-	rm ~/.zshrc
-	rm ~/.gitconfig
-	rm ~/.config/nvim/init.vim
+    # Remove Links
+    if [ -L ~/.vimrc ]; then 
+        rm ~/.vimrc
+    fi
+    if [ -L ~/.tmux.conf ]; then 
+        rm ~/.tmux.conf
+    fi
+    if [ -L ~/.zshrc ]; then 
+        rm ~/.zshrc
+    fi
+    if [ -L ~/.gitconfig ]; then 
+        rm ~/.gitconfig
+    fi
+    if [ -L ~/.config/nvim/init.vim ]; then 
+        rm ~/.config/nvim/init.vim
+    fi
+    
+    # Restore Backups
+    if [ -f ~/.backups/.vimrc.backup ]; then 
+        mv ~/.backups/.vimrc.backup ~/.vimrc 
+    fi
+    if [ -f ~/.backups/.gitconfig.backup ]; then 
+        mv ~/.backups/.gitconfig.backup ~/.gitconfig 
+    fi
+    if [ -f ~/.backups/.zshrc.backup ]; then 
+        mv ~/.backups/.zshrc.backup ~/.zshrc  
+    fi
+    if [ -f ~/.backups/.tmux.conf.backup ]; then 
+        mv ~/.backups/.tmux.conf.backup ~/.tmux.conf 
+    fi
+    if [ -f ~/.config/nvim/init.vim ]; then 
+        mv ~./.backups/.nvim.backup ~/.config/nvim/init.vim 
+    fi
 }
 
 if [[ $1 = "link" ]]
