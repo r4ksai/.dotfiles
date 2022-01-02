1a6d78f76c00b1b1e5f173de63303286222b571d
Sai <r4ksai@gmail.com>
2022-01-02 17:10:42 +0530

    🧲 Add persistance undo for vim
    Add folder undo inside .vim to enable persistance undo

--- setup.sh
+++ setup.sh
@@ -43,6 +43,7 @@ setup()
 	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 	# Vim
+	mkdir -p ~/.vim/undo
 	mkdir -p ~/.vim/pack/themes/start
 	cd ~/.vim/pack/themes/start
 	git clone https://github.com/dracula/vim.git dracula
