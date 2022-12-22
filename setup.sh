a759b49f6578ea23544fcab1533462146de17077
Sai <r4ksai@gmail.com>
2022-12-22 20:21:10 +0530

    🐢 Refactor setup with backup and linking functions

--- setup.sh
+++ setup.sh
@@ -17,11 +17,7 @@
         echo 'Updating'
         sudo apt update -qq
         echo 'Installing'
-        sudo apt install git vim tmux curl exa fzf ripgrep bat -y -qq
-        if [[ "$DOTINSTALL" != "server" ]]
-        then
-            sudo apt install neovim kitty nodejs npm -y -qq
-        fi
+        sudo apt install $(cat Aptlist) -y -qq
     fi
 }
 
@@ -30,11 +26,11 @@ setup()
 	# Install Packages
 	package_install
 
-    echo 'Select a default SHELL'
+    echo 'Configure default SHELL'
     echo '---------------------'
     echo ''
     echo '1. Bash'
-    echo '2. ZSH'
+    echo '2. Zsh'
     echo '3. Fish'
 
     while true; do
@@ -60,15 +56,13 @@ setup()
 	curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim 
 	mkdir ~/.vim/plugged
 
-    if [[ "$DOTINSTALL" != "server" ]]
-    then
-        npm install -g typescript typescript-language-server diagnostic-languageserver pyright prettier
+    # Language Support from Vim
+    npm install -g typescript typescript-language-server diagnostic-languageserver pyright prettier
 
-        # NeoVim
-        mkdir -p ~/.config/nvim/undo
-        curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-        pip3 install --user pynvim
-    fi
+    # NeoVim
+    mkdir -p ~/.config/nvim/undo
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    pip3 install --user pynvim
 
 	# Link files
 	link_files
@@ -76,12 +70,9 @@ setup()
 
 link_files()
 {
-    if [[ "$DOTINSTALL" != "server" ]]
-    then
-        # Create nvim directory
-        mkdir -p ~/.config/nvim
-        mkdir -p ~/.config/nvim/after/plugin
-    fi
+    # Create nvim directory
+    mkdir -p ~/.config/nvim
+    mkdir -p ~/.config/nvim/after/plugin
 
     # Clean
     clean
@@ -97,15 +88,28 @@ link_files()
     ln -s ~/.dotfiles/.inputrc ~/.inputrc
     ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 
-    if [[ "$DOTINSTALL" != "server" ]]
-    then
-        ln -s ~/.dotfiles/.vimrc ~/.vimrc
-        ln -s ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
-        ln -s ~/.dotfiles/plugin ~/.config/nvim/after/plugin
-    else
-        ln -s ~/.dotfiles/.vimrc-server ~/.vimrc
-    fi
+    ln -s ~/.dotfiles/.vimrc ~/.vimrc
+    ln -s ~/.dotfiles/.vimrc ~/.config/nvim/init.vim
+    ln -s ~/.dotfiles/plugin ~/.config/nvim/after/plugin
 
+}
+
+backupFiles() {
+    if [ -f ~/.$1 ]; then 
+        mv ~/.$1 ~/.backups/.$1.backup
+    fi
+}
+
+removeLinkedFiles() {
+    if [ -L $1]; then 
+        rm $1
+    fi
+}
+
+restoreBackupFiles() {
+    if [ -f ~/.backups/.$1.backup ]; then 
+        mv ~/.backups/.$1.backup ~/.$1 
+    fi
 }
 
 backup()
@@ -115,33 +119,18 @@ backup()
 	mkdir -p ~/.backups
 	cd ~/.backups
 
-    if [ -f ~/.vimrc ]; then 
-        mv ~/.vimrc ~/.backups/.vimrc.backup
-    fi
-    if [ -f ~/.gitconfig ]; then 
-        mv ~/.gitconfig ~/.backups/.gitconfig.backup 
-    fi
-    if [ -f ~/.bashrc ]; then 
-        mv ~/.bashrc ~/.backups/.bashrc.backup 
-    fi
-    if [ -f ~/.zshrc ]; then 
-        mv ~/.zshrc ~/.backups/.zshrc.backup 
-    fi
-    if [ -f ~/.inputrc ]; then 
-        mv ~/.inputrc ~/.backups/.inputrc.backup 
-    fi
-    if [ -f ~/.tmux.conf ]; then 
-        mv ~/.tmux.conf ~/.backups/.tmux.conf.backup 
-    fi
+    backupFiles vimrc
+    backupFiles tmux.conf
+    backupFiles bashrc
+    backupFiles zshrc
+    backupFiles inputrc
+    backupFiles gitconfig
 
-    if [[ "$DOTINSTALL" != "server" ]]
-    then
-        if [ -f ~/.config/nvim/init.vim ]; then 
-            mv ~/.config/nvim/init.vim ~./.backups/.nvim.backup
-        fi
-        if [ -f ~/.config/nvim/after/plugin ]; then 
-            mv ~/.config/nvim/after/plugin ~./.backups/.nvim.plugin.backup
-        fi
+    if [ -f ~/.config/nvim/init.vim ]; then 
+        mv ~/.config/nvim/init.vim ~./.backups/.nvim.backup
+    fi
+    if [ -f ~/.config/nvim/after/plugin ]; then 
+        mv ~/.config/nvim/after/plugin ~./.backups/.nvim.plugin.backup
     fi
 }
 
@@ -149,134 +138,104 @@ clean()
 {
     echo "Removing linked files"
     # Remove Links
-    if [ -L ~/.vimrc ]; then 
-        rm ~/.vimrc
-    fi
-    if [ -L ~/.tmux.conf ]; then 
-        rm ~/.tmux.conf
-    fi
-    if [ -L ~/.bashrc ]; then 
-        rm ~/.bashrc
-    fi
-    if [ -L ~/.zshrc ]; then 
-        rm ~/.zshrc
-    fi
-    if [ -L ~/.inputrc ]; then 
-        rm ~/.inputrc
-    fi
-    if [ -L ~/.gitconfig ]; then 
-        rm ~/.gitconfig
-    fi
+    removeLinkedFiles ~/.vimrc
+    removeLinkedFiles ~/.tmux.conf
+    removeLinkedFiles ~/.bashrc
+    removeLinkedFiles ~/.zshrc
+    removeLinkedFiles ~/.inputrc
+    removeLinkedFiles ~/.gitconfig
+    removeLinkedFiles ~/.config/nvim/init.vim
+    removeLinkedFiles ~/.config.nvim.after/plugin
 
-    if [[ "$DOTINSTALL" != "server" ]]
-    then
-        if [ -L ~/.config/nvim/init.vim ]; then 
-            rm ~/.config/nvim/init.vim
-        fi
-        if [ -L ~/.config/nvim/after/plugin ]; then 
-            rm -rf ~/.config/nvim/after/plugin
-        fi
-    fi
-    
     # Restore Backups
     echo "Restoring old files"
-    if [ -f ~/.backups/.vimrc.backup ]; then 
-        mv ~/.backups/.vimrc.backup ~/.vimrc 
+    restoreBackupFiles vimrc
+    restoreBackupFiles tmux.conf
+    restoreBackupFiles bashrc
+    restoreBackupFiles zshrc
+    restoreBackupFiles inputrc
+    restoreBackupFiles gitconfig
+
+    if [ -f ~/.config/nvim/init.vim ]; then 
+        mv ~./.backups/.nvim.backup ~/.config/nvim/init.vim 
     fi
-    if [ -f ~/.backups/.gitconfig.backup ]; then 
-        mv ~/.backups/.gitconfig.backup ~/.gitconfig 
+    if [ -f ~/.config/nvim/after/after/plugin ]; then 
+        mv ~./.backups/.nvim.plugin.backup ~/.config/nvim/after/plugin
     fi
-    if [ -f ~/.backups/.bashrc.backup ]; then 
-        mv ~/.backups/.bashrc.backup ~/.bashrc  
-    fi
-    if [ -f ~/.backups/.zshrc.backup ]; then 
-        mv ~/.backups/.zshrc.backup ~/.zshrc  
-    fi
-    if [ -f ~/.backups/.inputrc.backup ]; then 
-        mv ~/.backups/.inputrc.backup ~/.inputrc
-    fi
-    if [ -f ~/.backups/.tmux.conf.backup ]; then 
-        mv ~/.backups/.tmux.conf.backup ~/.tmux.conf 
-    fi
-    if [[ "$DOTINSTALL" != "server" ]]
+    }
+
+install_ohmyzsh() {
+    if [[ "$OSTYPE" == "darwin"* ]]
     then
-        if [ -f ~/.config/nvim/init.vim ]; then 
-            mv ~./.backups/.nvim.backup ~/.config/nvim/init.vim 
-        fi
-        if [ -f ~/.config/nvim/after/after/plugin ]; then 
-            mv ~./.backups/.nvim.plugin.backup ~/.config/nvim/after/plugin
+        brew install zsh
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]
+    then
+        sudo apt install zsh
+    fi
+
+    if [ -d ~/.oh-my-zsh ]; then
+        echo 'Oh my Zsh is already installed'
+    else
+        echo 'Installing Oh my Zsh'
+        sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+        source ~/.zshrc
+    fi
+
+    if ! command -v git &> /dev/null
+    then
+        echo 'Git not installed try again after installing git'
+    else
+        if [[ -z "${ZSH_CUSTOM }" ]]; then
+            echo 'Installing zsh suggestions and syntax highlighting without environment variable'
+            mkdir -p ~/.oh-my-zsh/custom
+            git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
+            git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+        else
+            echo 'Installing zsh suggestions and syntax highlighting'
+            git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
+            git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
         fi
     fi
-    }
+}
 
-    install_ohmyzsh() {
-        if [[ "$OSTYPE" == "darwin"* ]]
-        then
-            brew install zsh
-        elif [[ "$OSTYPE" == "linux-gnu"* ]]
-        then
-            sudo apt install zsh
-        fi
+install_ohmybash() {
+    if [[ "$OSTYPE" == "darwin"* ]]
+    then
+        brew install bash
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]
+    then
+        sudo apt install bash
+    fi
 
-        if [ -d ~/.oh-my-zsh ]; then
-            echo 'Oh my Zsh is already installed'
-        else
-            echo 'Installing Oh my Zsh'
-            sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-            source ~/.zshrc
-        fi
+    if [ -d ~/.oh-my-bash ]; then
+        echo 'Oh my Bash is already installed'
+    else
+        echo 'Installing Oh my Bash'
+        bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+        source ~/.bashrc
+    fi
+}
 
-		if ! command -v git &> /dev/null
-        then
-            echo 'Git not installed try again after installing git'
-        else
-            if [[ -z "${ZSH_CUSTOM }" ]]; then
-                echo 'Installing zsh suggestions and syntax highlighting without environment variable'
-                mkdir -p ~/.oh-my-zsh/custom
-                git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
-                git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-            else
-                echo 'Installing zsh suggestions and syntax highlighting'
-                git clone https://github.com/zsh-users/zsh-autosuggestions.git $ZSH_CUSTOM/plugins/zsh-autosuggestions
-                git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-            fi
-        fi
-    }
+install_ohmyfish() {
+    if [[ "$OSTYPE" == "darwin"* ]]
+    then
+        brew install fish
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]
+    then
+        sudo apt install fish
+    fi
 
-    install_ohmybash() {
-        if [[ "$OSTYPE" == "darwin"* ]]
-        then
-            brew install bash
-        elif [[ "$OSTYPE" == "linux-gnu"* ]]
-        then
-            sudo apt install bash
-        fi
+    if [ -d ~/.oh-my-fish ]; then
+        echo 'Oh my Fish is already installed'
+    else
+        echo 'Installing Oh my Fish'
+        curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish 
+    fi
+}
 
-        if [ -d ~/.oh-my-bash ]; then
-            echo 'Oh my Bash is already installed'
-        else
-            echo 'Installing Oh my Bash'
-            bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-            source ~/.bashrc
-        fi
-    }
-
-    install_ohmyfish() {
-        if [[ "$OSTYPE" == "darwin"* ]]
-        then
-            brew install fish
-        elif [[ "$OSTYPE" == "linux-gnu"* ]]
-        then
-            sudo apt install fish
-        fi
-
-        if [ -d ~/.oh-my-fish ]; then
-            echo 'Oh my Fish is already installed'
-        else
-            echo 'Installing Oh my Fish'
-            curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish 
-        fi
-    }
+######################################################################################
+#   Dotfile Installation Setup
+######################################################################################
 
 echo 'Dotfiles installation'
 echo '---------------------'
@@ -285,8 +244,7 @@ echo '1. Setup Dotfiles'
 echo '2. Link Dotfiles'
 echo '3. Install Supporting deps'
 echo '4. Clean Dotfiles'
-echo '5. Setup Dotfiles for Server'
-echo '6. Exit'
+echo '5. Exit'
 while true; do
     read -p 'Select option [1,7]: ' option
     case $option in
@@ -294,8 +252,7 @@ while true; do
         2 ) link_files; break;;
         3 ) package_install; break;;
         4 ) clean; break;;
-        5 ) DOTINSTALL='server';setup; break;;
-        6 ) exit;;
+        5 ) exit;;
         * ) echo "Please select a valid input !";;
     esac
 done
