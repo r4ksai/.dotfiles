e1bd73fc6aca786769b4b63faacb71b32f552d65
Sai <r4ksai@gmail.com>
2023-01-28 22:34:08 +0530

    🐮 Add language server for nvim in plugins

--- nvim/ftplugin/python.lua
+++ nvim/ftplugin/python.lua
@@ -1,7 +1,7 @@
 -- Python
 
 -- Format code, -> Change to only if yapf command exists
-a.nvim_create_user_command('Format', '0,$!yapf', {})
+-- a.nvim_create_user_command('Format', '0,$!yapf', {})
 
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
