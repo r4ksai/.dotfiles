596ae1374432a829706a3193bc55829d7c31f503
Sai <r4ksai@gmail.com>
2024-01-27 17:36:26 +0530

    💦 Add CTR+N Autocompletion with NEXT and PREVIOUS Controls

---
+++ lazyvim/lua/plugins/coding.lua
@@ -0,0 +1,32 @@
+return {
+  "hrsh7th/nvim-cmp",
+  dependencies = {
+    "hrsh7th/cmp-emoji",
+  },
+  ---@param opts cmp.ConfigSchema
+  opts = function(_, opts)
+    local luasnip = require("luasnip")
+    local cmp = require("cmp")
+
+    opts.mapping = vim.tbl_extend("force", opts.mapping, {
+      ["<C-n>"] = function(_)
+        if cmp.visible() then
+          cmp.select_next_item({ behavior = cmp.SelectBehavior.Insert })
+        elseif luasnip.expand_or_jumpable() then
+          luasnip.expand_or_jump()
+        else
+          cmp.complete()
+        end
+      end,
+      ["<C-p>"] = function(_)
+        if cmp.visible() then
+          cmp.select_prev_item({ behavior = cmp.SelectBehavior.Insert })
+        elseif luasnip.jumpable(-1) then
+          luasnip.jump(-1)
+        else
+          cmp.complete()
+        end
+      end,
+    })
+  end,
+}
